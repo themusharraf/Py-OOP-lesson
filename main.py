60897a9ec059c7d7071d33c6eec5a1c2d8cc4d57
allnc <meibrohimov@gmail.com>
2023-12-18 10:09:03 +0500

    👨‍💻

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-from colorama import Fore
+# from colorama import Fore
+#
+#
+# class Car:
+#     name = ""
+#     color = ""
+#     price = 0
+#
+#
+# car = Car()  # object 1
+# car.name = "BMW X7"
+# car.color = "black"
+# car.price = 145_000
+#
+# car1 = Car()  # object 2
+# car1.name = "BMW X"
+# car1.color = "Green"
+# car1.price = 34_900
+#
+# print(Fore.RED + "Car name:" + car.name)
+# print(Fore.YELLOW + "Car color:", car.color)
+# print(Fore.GREEN + "Car price:", car.price)
 
 
-class Car:
-    name = ""
-    color = ""
-    price = 0
+class Person:
+    def __init__(self, name, age, gender, address):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.address = address
 
 
-car = Car()  # object 1
-car.name = "BMW X7"
-car.color = "black"
-car.price = 145_000
-
-car1 = Car()  # object 2
-car1.name = "BMW X"
-car1.color = "Green"
-car1.price = 34_900
-
-print(Fore.RED + "Car name:" + car.name)
-print(Fore.YELLOW + "Car color:", car.color)
-print(Fore.GREEN + "Car price:", car.price)
+Odam = Person("Barno", 18, "qiz", "Tashkent")
