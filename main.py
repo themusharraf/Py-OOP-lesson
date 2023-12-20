a48cdcee886c26d8f373c8433cf5333c6b756dae
allnc <meibrohimov@gmail.com>
2023-12-20 10:11:35 +0500

    new method 💡

--- main.py
+++ main.py
@@ -20,21 +20,19 @@
 # print(Fore.RED + "Car name:" + car.name)
 # print(Fore.YELLOW + "Car color:", car.color)
 # print(Fore.GREEN + "Car price:", car.price)
-
-
 class Person:
-    def __init__(self, name, year, gender, address):
-        self.name = name
-        self.year = year
-        self.gender = gender
-        self.address = address
+    # Construct or Instance method
+    def __init__(self, first_name, last_name, age, address):
+        self.first_name = first_name  # instance attribute
+        self.last_name = last_name  # instance attribute
+        self.age = age  # instance attribute
+        self.address = address  # instance attribute
 
+    # custom class method
     def get_info(self):
-        return self.name, self.year, self.gender, self.address
+        return self.first_name, self.last_name, self.age, self.address
 
 
-
-
-Odam = Person("Barno", 2000, "qiz", "Tashkent")
-
-print(*Odam.get_info())
+# object
+odam = Person("Musharraf", "Ibragimov", 23, "Tashkent")
+print(*odam.get_info())
