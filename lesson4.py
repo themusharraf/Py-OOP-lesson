cc003a5607d1dd8594fe7acc0a91b044286de10f
allnc <meibrohimov@gmail.com>
2023-12-22 09:48:07 +0500

    new method 💡

---
+++ lesson4.py
@@ -0,0 +1,18 @@
+class Shaxs:
+    def __init__(self, ism, famlily, age, passport, gender):
+        self.ism = ism
+        self.famlily = famlily
+        self.age = age
+        self.passport = passport
+        self.gender = gender
+
+    def get_passport(self):
+        return self.passport
+
+
+obj = Shaxs("Akbar", "Akbarov", 27, "AB123434", "Male")
+
+
+class
+
+
