452fdfbd42f51c4e3b384c05ef726148c3bda6c6
allnc <meibrohimov@gmail.com>
2023-12-23 10:44:15 +0500

    add file

--- lesson7.py
+++ lesson7.py
@@ -6,7 +6,7 @@ Point.name
 
 """
 
-
+"""
 class Point:
     name = None
 
@@ -15,3 +15,47 @@ class Point:
         Point.name = newname
 
 
+print(Point.name)
+Point.change_name("PDP point")
+print(Point.name)
+
+"""
+
+
+class Person:
+    def __init__(self, firstname, lastname, year, address):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.year = year
+        self.address = address
+
+
+class Student(Person):
+    def __init__(self, firstname, lastname, year, address, student_id, username, password, university):
+        super().__init__(firstname, lastname, year, address)
+        self.student_id = student_id
+        self.username = username
+        self.password = password
+        self.university = university
+
+
+    def age(self):
+        ...
+
+
+
+student = Student("Akbar", "Alimov", 2008, "Tashkent", "007", "@akbar9", "Ass888", "PDP")
+print(student.age)
+
+
+
+
+"""
+class Student():
+
+University name
+student_id
+username
+password
+
+"""
