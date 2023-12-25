cef94db221bc4a3c5d77b1ff753b2a65a511bdae
allnc <meibrohimov@gmail.com>
2023-12-25 10:11:19 +0500

    add file

--- lesson8.py
+++ lesson8.py
@@ -12,5 +12,6 @@ class Person:
 
 
 objects = Person("Alice", "16", "Tashkent")
-print(objects.get_address())  # getter for the address
-
+print(objects.get_address())  # getter
+objects.set_address("Samarkand") # setter
+print(objects.get_address()) # setter
