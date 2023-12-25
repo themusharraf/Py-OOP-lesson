bdb97292535a63e27fab08cf482694b320131d3b
allnc <meibrohimov@gmail.com>
2023-12-25 09:59:03 +0500

    add file

---
+++ lesson8.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, name, age, address):
+        self.name = name
+        self.age = age
+        self.address = address
+
+    def get_address(self):  # getter for the address
+        return self.address
+
+
+objects = Person("Alice", "16", "Tashkent")
