4c35b4f7f1f2e9c03f3d9843700b82149b954720
allnc <meibrohimov@gmail.com>
2023-12-25 10:10:35 +0500

    add file

--- lesson8.py
+++ lesson8.py
@@ -4,9 +4,13 @@ class Person:
         self.age = age
         self.address = address
 
-    def get_address(self):  # getter for the address
+    def get_address(self):  # getter
         return self.address
+
+    def set_address(self, new_address):
+        self.address = new_address # setter
 
 
 objects = Person("Alice", "16", "Tashkent")
 print(objects.get_address())  # getter for the address
+
