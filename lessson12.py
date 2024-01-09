1fa88195fc952ce990b854cd494bcbd3c50fa737
msi <meibrohimov.@gmail.com>
2024-01-09 11:49:01 +0500

    # Polymorphism

--- lessson12.py
+++ lessson12.py
@@ -1,24 +1,60 @@
 # Polymorphism (Ko'p shakllilik) # noqa
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
 
-    # method
-    def info(self):
-        return self.name, self.age
-
-
-class student(Person):
-    def __init__(self, name, age, username, id):
-        super().__init__(name, age)
-        self.username = username
-        self.id = id
+# Overloading
+"""
+Overloading, bir sinfdagi biror funksiya yoki
+metod nomini bir nechta turdagi argumentlar
+bilan o'zgartirish imkoniyatini anglatadi. Misol uchun:
+"""
 
 
-    def info(self):
-        return self.username, self.id, self.name, self.age
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def add(self, a, b, c):
+        return a + b + c
 
 
-one = student('Said', 18, "@said007", 402)
-print(*one.info())
+one = Calculator()
+print(one.add(1, 3, 4))
+
+# Overriding
+"""
+Overriding esa ota sinfdagi metodni farzand
+sinfda qayta yozishni ifodalaydi. Misol uchun:
+"""
+
+
+class Shape:
+    def draw(self):
+        print("Drawing a shape")
+
+
+class Circle(Shape):
+    def draw(self):
+        print("Drawing a circle")
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     # method
+#     def info(self):
+#         return self.name, self.age
+#
+#
+# class student(Person):
+#     def __init__(self, name, age, username, id):
+#         super().__init__(name, age)
+#         self.username = username
+#         self.id = id
+#
+#
+#     def info(self):
+#         return self.username, self.id, self.name, self.age
+#
+#
+# one = student('Said', 18, "@said007", 402)
+# print(*one.info())
