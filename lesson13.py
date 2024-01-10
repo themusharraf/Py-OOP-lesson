bb4899fdb09210b93dc4ff56a40218a293539a08
msi <meibrohimov.@gmail.com>
2024-01-10 10:01:50 +0500

    # Encapsulation

---
+++ lesson13.py
@@ -0,0 +1,11 @@
+
+# Encapsulation
+class Shaxs:
+    def __init__(self, ism, yosh, idcard):
+        self.ism = ism
+        self.yosh = yosh
+        self.__idcard = idcard
+
+
+shaxs = Shaxs("Anvar", 24, "AB23567")
+print(shaxs.idcard)
