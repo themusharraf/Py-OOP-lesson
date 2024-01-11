fd0fbdadb7f61f46fa7cbc549b748d3c13c21fbb
msi <meibrohimov.@gmail.com>
2024-01-11 09:39:18 +0500

    # Polymorphism 1 ->  overloading 2 -> overriding

--- lesson15.py
+++ lesson15.py
@@ -4,18 +4,34 @@ Polymorphism
 2 -> overriding
 """
 
-
-class Calculator:
-    def add(self, a, b):
-        print(a + b)
-
-    def add(self, a, b, c):
-        print(a + b + c)
-
-
-obj = Calculator()
+# class Calculator:
+#     def add(self, a, b):
+#         print(a + b)
+#
+#     def add(self, a, b, c):
+#         print(a + b + c)
+#
+#
+# obj = Calculator()
 # obj.add(2, 4, 4)
 
 a = 4
 a = 5
+
 # print(a)
+
+# overriding
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+class Phone(Calculator):
+    def __init__(self,number):
+        super().__init__()
+        self.number = number
+
+    def add(self, a, b):
+        return a - b
+
+con = Phone(2)
+print(con.add(7,5))
