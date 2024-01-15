3835876459c348d368755eabdca967bafffed524
msi <meibrohimov.@gmail.com>
2024-01-15 10:14:43 +0500

    #DataClass

--- lesson19.py
+++ lesson19.py
@@ -7,8 +7,8 @@ class Xodim:
     age: int
 
     emp_id: int
-    city: str = field(init=False, default="Tashkent")
+    city: str = field(init=False, default="Tashkent", repr=True)
 
 
 obj = Xodim("Alice", 18, 4456)
-print(obj.city)
+print(obj)
